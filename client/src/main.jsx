bf0f9b8768ada456af6a07114669cbd8cd316bca
devltt404 <devltt404@usf.edu>
2024-09-29 13:11:32 -0400

    chore: edit config to deploy to git

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import { store } from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ReduxProvider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="devshop/">
       <App />
     </BrowserRouter>
   </ReduxProvider>,
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,10 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
 import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/devshop/",
   plugins: [react()],
   resolve: {
     alias: {
