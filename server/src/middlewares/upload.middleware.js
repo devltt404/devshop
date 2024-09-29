53ef130d257269ae4d789e2ce39bace97eed2786
devltt404 <devltt404@usf.edu>
2024-09-29 13:27:31 -0400

    refactor: remove the use of multer-storage-cloudinary

--- server/src/middlewares/upload.middleware.js
+++ server/src/middlewares/upload.middleware.js
@@ -1,38 +1,44 @@
-import { v2 as cloudinary } from "cloudinary";
 import multer from "multer";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
 import { CLOUDINARY } from "../constants/index.js";
 import logger from "../logger.js";
 
-export const uploadCloud = ({ folderName, allowedFormats }) => {
-  const storage = new CloudinaryStorage({
-    cloudinary,
-    allowedFormats,
-    params: {
-      folder: `devshop/${folderName}`,
-    },
-  });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); // Temporary storage location
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
 
+export const uploadCloud = ({ folderName, allowedFormats }) => {
   return multer({
     storage,
+    fileFilter: (req, file, cb) => {
+      // Check for allowed formats
+      const ext = file.mimetype.split("/")[1];
+      if (!allowedFormats.includes(ext)) {
+        return cb(new Error("Invalid file format"), false);
+      }
+      cb(null, true);
+    },
     limits: {
       fileSize: CLOUDINARY.MAX_SIZE, // keep images size < 5 MB
     },
   });
 };
 
-// Middleware to log file upload details
 export const logUploadDetails = (req, res, next) => {
   if (req.file) {
     logger.info(
-      `File uploaded to Cloudinary: ${req.file.filename} (${req.file.size} bytes)`
+      `File uploaded to Cloudinary: ${req.file.cloudinaryUrl} (${req.file.size} bytes)`
     );
   }
- 
+
   if (req.files) {
     req.files.forEach((file) => {
       logger.info(
-        `File uploaded to Cloudinary: ${file.filename} (${file.size} bytes)`
+        `File uploaded to Cloudinary: ${file.cloudinaryUrl} (${file.size} bytes)`
       );
     });
   }
