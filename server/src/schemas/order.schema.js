fc1b34f894f9ce1bfc5ad554fe958e50b618e1f1
devltt404 <devltt404@usf.edu>
2024-10-06 18:48:35 -0400

    fix: update optional fields for createOrderSchema

--- server/src/schemas/order.schema.js
+++ server/src/schemas/order.schema.js
@@ -4,7 +4,7 @@ export const createOrderSchema = Joi.object({
   paymentIntentId: Joi.string().required(),
   customerInfo: Joi.object({
     name: Joi.string().required(),
-    phone: Joi.string().required(),
+    phone: Joi.string().optional(),
   }).required(),
-  shippingAddress: Joi.object().pattern(Joi.string(), Joi.string()).required(),
+  shippingAddress: Joi.object().pattern(Joi.string(), Joi.any()).required(),
 });
